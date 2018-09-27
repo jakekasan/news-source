cd6feee0f7b6c04a2545cf45225affb900593576
jakekasan <jake.kasan@gmail.com>
2018-09-27 22:28:03 +0100

    added data import and data_handler

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+from data_handler import Data_Handler
 from news_wrangler import News_Wrangler
 from guardian import Guardian
 from config import config
+import datetime as dt
+import pandas as pd
+import numpy as np
 
 def main():
-    guardian_config = config["sources"]["guardian"]
-    
     guardian = Guardian()
 
-    print(guardian.search("RBS"))
+    df = pd.read_csv("./data/RBS.csv")
+
+    print(df.head())
+
+    df["date"] = df["Date"].apply(lambda x: dt.datetime.strptime(x,"%Y-%M-%d"))
+
+    print(df.head())
+
+    df["name"] = df.apply(lambda x: "RBS",axis=1)
+
+    dh = Data_Handler()
+
+    results = dh.run(df)
+
+    print(results)
 
 if __name__ == '__main__':
     main()
