63cdaabfd18ff3c385bfd3086476ce25b902802b
jakekasan <jake.kasan@gmail.com>
2018-09-27 19:21:31 +0100

    does a simple search for 'RBS'

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from news_wrangler import News_Wrangler
+from guardian import Guardian
 from config import config
 
 def main():
     guardian_config = config["sources"]["guardian"]
-    guardian = News_Wrangler(guardian_config["address"],guardian_config["API_KEY"])
+    
+    guardian = Guardian()
 
-    guardian.search("RBS")
+    print(guardian.search("RBS"))
 
 if __name__ == '__main__':
     main()
