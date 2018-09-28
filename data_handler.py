6e1836d08dd541adf2af9536b99b07571e77ee9c
jakekasan <jake.kasan@gmail.com>
2018-09-28 13:48:10 +0100

    added check to see if text was already found for that day and stock

--- data_handler.py
+++ data_handler.py
@@ -14,22 +14,41 @@ class Data_Handler:
     def run(self,df=None):
         print("Started processing data for {}".format(df["name"][0]))
         print("Df size:",df.shape)
+        df = self.process_prices(df,window=1)
         df = df.apply(lambda x: self.process_row(x) ,axis=1)
         print("Finished processing data")
         return df
 
     def process_row(self,row):
-        results = ""
-
+        """
+            Takes a row, applies search for each source, and returns row
+        """
         for source in self.sources:
-            text = source.search(row["name"],date=row["date"])
-            results = "".join([results,text])
+            col_name = "raw_text_{}".format(source.name)
 
-        row["raw_text"] = results
+            if col_name in row.index and row[col_name] != "":
+                continue
+        
+            row[col_name] = source.search(row["name"],date=row["date"])
 
         print("Finished {}".format(row["date"]))
 
         return row
+
+    def process_prices(self,df,window=1):
+        """
+            Calculates return and then applies smoothing
+        """
+        df["price"] = df["Close"].pct_change(periods=window)
+
+        df["price"].fillna(0,inplace=True)
+
+        return df
+
+        
+
+        
+    
 
 
         
