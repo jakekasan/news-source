933269ce908d2daa0e3fabbc0424aaced1af30be
jakekasan <jake.kasan@gmail.com>
2018-09-28 13:50:36 +0100

    started working getting companies lists from data directory

--- main.py
+++ main.py
@@ -2,12 +2,34 @@ from data_handler import Data_Handler
 from news_wrangler import News_Wrangler
 from guardian import Guardian
 from config import config
+from ticker_lookup import ticker_lookup
+
 import datetime as dt
-import pandas as pd
 import numpy as np
+import os
+import pandas as pd
+import re
+
+
 
 def main():
-    guardian = Guardian()
+
+    pwd = os.getcwd()
+
+    if "data" not in [x for x in os.listdir(pwd) if os.path.isdir(x)]:
+        print("No data directory")
+        return
+
+    data_dir = os.path.join(pwd,"data")
+
+    csv_regex = re.compile(r".*\.csv")
+
+    for f in os.listdir(data_dir):
+        match = csv_regex.search(f)
+        print(match.group())
+    return
+    #ticker_lookup("RBS")
+    return
 
     df = pd.read_csv("./data/RBS.csv")
 
@@ -19,11 +41,13 @@ def main():
 
     df["name"] = df.apply(lambda x: "RBS",axis=1)
 
+    df = df.head(20)
+
     dh = Data_Handler()
 
     results = dh.run(df)
 
-    print(results)
+    print(results.tail(1)["raw_text_guardian"].values)
 
 if __name__ == '__main__':
     main()
