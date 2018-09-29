540621f04dac10b9793e53ff3fd2c474505418a7
jakekasan <jake.kasan@gmail.com>
2018-09-29 11:11:14 +0100

    modified files so that module can be imported

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from tools.data_handler import Data_Handler
+from tools.ticker_lookup import ticker_lookup
+from sources.guardian import Guardian
--- config.py
+++
@@ -1,9 +0,0 @@
-config = {
-    "sources":{
-        "bbc":"BBC_API",
-        "guardian":{
-            "address":"https://content.guardianapis.com/search",
-            "API_KEY":"c02f338f-9587-4500-b9b9-06351534443a"
-        }
-    }
-}
