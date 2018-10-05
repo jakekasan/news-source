28f872036e5802072958acb8388b0307d4bfd9ac
jakekasan <jake.kasan@gmail.com>
2018-10-05 13:55:24 +0100

    added tests for main function (yet to be implemented)

--- test.py
+++ test.py
@@ -61,7 +61,11 @@ class Import_Guardian(unittest.TestCase):
 
         self.assertTrue(cond)
     
-     
+class Import_Main(unittest.TestCase):
+    def test_main_exists(self):
+        from main import main
+
+        self.assertIsInstance(main,object)
 
 if __name__ == '__main__':
     unittest.main()
