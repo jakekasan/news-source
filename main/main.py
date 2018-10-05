425416e5f30ba161a474e3cdea21a8b582d621bf
jakekasan <jake.kasan@gmail.com>
2018-10-05 14:01:25 +0100

    added functions to Manager

--- main/main.py
+++ main/main.py
@@ -1,11 +1,18 @@
+"""
+    Find a way of importing a dynamic amount of sources
+"""
+
 from sources.guardian import Guardian
 
 class Manager():
     def __init__(self):
         pass
 
-    def get_text(self,string,data_from=None,date_to=None):
+    def get_text(self,query=None,data_from=None,date_to=None):
         """
             returns a pd.Series with raw text from all the sources
         """
+        pass
+
+    def get_list_of_text(self):
         pass
