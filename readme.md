ac439e3b409503a2cfd78f349db169ad6c2c9003
jakekasan <jake.kasan@gmail.com>
2018-10-05 13:16:35 +0100

    added setters for api_address and api_key

--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 ## About
 
 This module serves to help import raw text data from news sources (at the moment, just the Guardian)
+
--- sources/guardian.py
+++ sources/guardian.py
@@ -1,6 +1,6 @@
-from config.news_sources import guardian as api
+#from config.news_sources import guardian as api
 import requests
-from news_source.sources.news_wrangler import News_Wrangler
+from sources.news_wrangler import News_Wrangler
 
 address = "https://content.guardianapis.com/search"
 
@@ -9,8 +9,14 @@ class Guardian(News_Wrangler):
         super()
         self.debug = debug
         self.name = "guardian"
-        self.address = api["address"]
-        self.API_KEY = api["API_KEY"]
+        self.address = None #api["address"]
+        self.api_key = None #api["API_KEY"]
+
+    def set_api_address(self,address=None):
+        self.address = address
+
+    def set_api_key(self,key=None):
+        self.api_key = key
 
     def api_search(self,search,from_date=None,to_date=None,news_delay=None,just_results=False):
         """
@@ -24,7 +30,7 @@ class Guardian(News_Wrangler):
             news_delay = 3
 
         params = {
-            "api-key":self.API_KEY,
+            "api-key":self.api_key,
             "q":"{} OR (royal AND bank AND of AND scotland) OR (brexit) OR (mark AND carney) OR economy OR unemployment OR".format(search),
             "from-date":from_date,
             "to-date":to_date,
