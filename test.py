cd1c784fbc507d3bf0f5d26ea1da8df689803ee1
jakekasan <jake.kasan@gmail.com>
2018-10-05 13:26:31 +0100

    added attribute tests

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 
-class Import_News_Wrangler(unittest.TestCase):
+class Import_Guardian(unittest.TestCase):
     """
         Tests that you can set config
     """
@@ -17,8 +17,51 @@ class Import_News_Wrangler(unittest.TestCase):
         from sources.guardian import Guardian
 
         g = Guardian()
-        
+
         self.assertIsInstance(g,Guardian)
+
+    def test_guardian_instance_has_name_attr(self):
+        from sources.guardian import Guardian
+
+        g = Guardian()
+
+        self.assertIsInstance(g.name,str)
+
+    def test_guardian_instance_name_is_guardian(self):
+        from sources.guardian import Guardian
+
+        g = Guardian()
+
+        self.assertEqual(g.name,"guardian")
+
+    def test_guardian_instance_has_attr_set_api_address(self):
+        from sources.guardian import Guardian
+
+        g = Guardian()
+
+        cond = hasattr(g,"set_api_address")
+
+        self.assertTrue(cond)
+
+    def test_guardian_instance_has_attr_set_api_key(self):
+        from sources.guardian import Guardian
+
+        g = Guardian()
+
+        cond = hasattr(g,"set_api_key")
+
+        self.assertTrue(cond)
+
+    def test_guardian_instance_has_attr_api_search(self):
+        from sources.guardian import Guardian
+
+        g = Guardian()
+
+        cond = hasattr(g,"api_search")
+
+        self.assertTrue(cond)
+    
+     
 
 if __name__ == '__main__':
     unittest.main()
