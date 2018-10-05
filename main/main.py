3e31237c8caa2cf9a7a0736c4d084f50a40a83a3
jakekasan <jake.kasan@gmail.com>
2018-10-05 13:55:34 +0100

    started work on main function

---
+++ main/main.py
@@ -0,0 +1,11 @@
+from sources.guardian import Guardian
+
+class Manager():
+    def __init__(self):
+        pass
+
+    def get_text(self,string,data_from=None,date_to=None):
+        """
+            returns a pd.Series with raw text from all the sources
+        """
+        pass
