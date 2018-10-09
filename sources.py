01133ca725d211c03c23452c62741f167255ae55
jakekasan <jake.kasan@gmail.com>
2018-10-09 17:43:44 +0100

    added config function for loading api_keys from file

--- sources.py
+++ sources.py
@@ -2,16 +2,43 @@
     sources.py
 """
 
-def guardian_config(source=None):
+import os
+import sys
+
+def config(source=None):
     """
         looks for file with API keys
     """
 
     # take 
+    if source is None:
+        return None
+
+    if "api.keys" not in os.listdir():
+        return None
+
+    with open("api.keys","w") as f:
+        raw_lines = f.readlines()
+
+        api_keys = {}
+        for line in raw_lines:
+            data = line.split(":")
+            name, key = data[0], data[1]
+            api_keys[name] = key
+
+        if source in api_keys.keys():
+            return api_keys[source]
+        return None
+        
+        
+
 
 
 def guardian_lookup(query=None,date_from=None,date_to=None):
     """
         returns a list of articles for the given query, date_from, date_to
     """
+    key = config(source="guardian")
 
+    if key is None:
+        return None
