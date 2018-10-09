5e0f1ba4210c063e329ceaf807488fc91b8e2921
jakekasan <jake.kasan@gmail.com>
2018-10-09 17:31:28 +0100

    added get_date_range function

--- tools.py
+++ tools.py
@@ -1,10 +1,17 @@
 import datetime as dt
 
-def get_date_range(date=None,range=3):
+def get_date_range(date=None,day_range=3):
     """
         takes a date (string or datetime object) and returns date_from and date_to
     """
     if date is None:
         date = None
 
-    
+    if type(date) != dt.datetime:
+        date = dt.datetime.strptime(date)
+
+    date_to = date
+    date_from = date_to - dt.timedelta(days=day_range)
+
+    return date_from,date_to
+
