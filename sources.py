d881643a18babbd38e23a5e362cdaed9bb72a483
jakekasan <jake.kasan@gmail.com>
2018-10-09 17:31:09 +0100

    new file for source APIs

---
+++ sources.py
@@ -0,0 +1,17 @@
+"""
+    sources.py
+"""
+
+def guardian_config(source=None):
+    """
+        looks for file with API keys
+    """
+
+    # take 
+
+
+def guardian_lookup(query=None,date_from=None,date_to=None):
+    """
+        returns a list of articles for the given query, date_from, date_to
+    """
+
