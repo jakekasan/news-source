1f658b0efc3e1280eacc7fd5b684176d6e4b2339
jakekasan <jake.kasan@gmail.com>
2018-10-12 13:26:24 +0100

    added function for quickly getting datetime pair from string date

--- tools.py
+++ tools.py
@@ -2,16 +2,23 @@ import datetime as dt
 
 def get_date_range(date=None,date_range=3):
     """
-        takes a date (string or datetime object) and returns date_from and date_to
+        takes a date (string or datetime object)
+        
+        returns date_from, date_to
     """
     if date is None:
         date = None
 
-    if type(date) != dt.datetime:
-        date = dt.datetime.strptime(date)
+    #if type(date) != dt.datetime:
+        #date = dt.datetime.strptime(date)
 
     date_to = date
     date_from = date_to - dt.timedelta(days=date_range)
 
     return date_from,date_to
+
+def get_date_pair_from_str(date_str=None,date_range=3):
+    return get_date_range(date=dt.datetime.strptime(date_str,"%Y-%m-%d"),date_range=date_range)
+    
+
 
