3e20c969f3c8d49b8cfc116b1751268d9684c380
jakekasan <jake.kasan@gmail.com>
2018-10-12 10:57:37 +0100

    added function to apply to df

--- main.py
+++ main.py
@@ -4,11 +4,14 @@
 
 #from sources.guardian import Guardian
 #from sources import all_sources
-from sources import sources_list
+from sources import all_sources
+from tools import get_date_range
+
 import datetime as dt
+import pandas as pd
 
 
-def single_lookup(query=None,date_from=None,date_to=None,array=False):
+def single_lookup(query=None,date_from=None,date_to=None,articles=False,day_range=3):
     """
         returns a single string of news articles based on the query and dates
         
@@ -22,16 +25,19 @@ def single_lookup(query=None,date_from=None,date_to=None,array=False):
             date_from = dt.datetime.now()
             date_to = dt.datetime.now()
 
-    articles = []
+    article_list = []
 
     for source in sources_list:
         s = source()
-        articles += s.single_lookup(query=query)
+        article_list += s.single_lookup(query=query,date_from=date_from,date_to=date_to)
+
+    if not articles:
+        return articles
 
 
     
 
-def range_lookup(query=None,df=None):
+def range_lookup(query=None,df=None,articles=False,date_range=3):
     """
         returns a pd.Series of results for a range of dates for the given query
     """
@@ -55,10 +61,32 @@ def range_lookup(query=None,df=None):
 
     # Do the magic
 
+    def get_results_for_row(row,query=None,source=None):
+        date_to = row["date"]
+        date_from = date_to - dt.timedelta(days=date_range)
+
+        return source(query=query,date_from=date_from,date_to=date_to)
+        
+        
+
+    for key,item in all_sources.items():
+        df["{}_article_list".format(key)] = df.apply(lambda x: get_results_for_row(row=x,query=query,source=item),axis=1)
+
+    return df
+
+
+
+
+    
+
 def main():
-    for thing in sources_list:
-        newthing = thing()
-        print(newthing.name)
+
+    df = pd.read_csv("./data/RBS.csv")
+    data = df.sample(10)
+
+    for key,item in all_sources.items():
+        print(key,item)
+    print(data)
 
 if __name__ == '__main__':
     main()
